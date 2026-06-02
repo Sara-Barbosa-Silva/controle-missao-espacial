f2523b652cb21fa02d021dbb9a2465688ff59551
Sara-Barbosa-Silva <sara.barbosa.silvas@gmail.com>
2026-06-02 18:12:08 -0300

    feat: cria backend spring boot

---
+++ backend-missao/src/main/java/com/fiap/ec/backend_missao/BackendMissaoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.backend_missao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMissaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMissaoApplication.class, args);
+	}
+
+}
---
+++ backend-missao/src/test/java/com/fiap/ec/backend_missao/BackendMissaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.backend_missao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMissaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
